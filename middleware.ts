7371478de089578c7c0878bdda634091941c7c16
Manish Suthar <manishsuthar078@gmail.com>
2024-06-22 17:39:56 +0530

    added route and clerk auth

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/ask-question"]);
+// protecting routes
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
