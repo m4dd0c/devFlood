2a5ec8fcde3a1ebe48edf349b878f48cab405fde
Manish Suthar <manishsuthar078@gmail.com>
2024-06-26 00:15:09 +0530

    changed middleware.ts temp

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -5,103 +5,109 @@ import { NextResponse } from "next/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the 
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-  
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
-  }
-
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt: WebhookEvent;
-
-  // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
-
-  const eventType = evt.type;
-
-  if (eventType === "user.created") {
-    try {
-      console.log('user.created up')
-      const userData = {
-        clerkId: evt.data.id,
-        email: evt.data.email_addresses[0].email_address,
-        username: evt.data.username!,
-        name: `${evt.data.first_name} ${evt.data.last_name ? evt.data.last_name : ""}`,
-        picture: evt.data.image_url,
-      };
-      console.log('user.created mid')
-      const user = await createUser(userData);
-      console.log('user.created down')
-      return NextResponse.json({ message: "OK", user });
-    } catch (error) {
-      console.error("webhook.user.created:", error);
+    // You can find this in the Clerk Dashboard -> Webhooks -> choose the
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+    console.log("wbh+sec", WEBHOOK_SECRET);
+    if (!WEBHOOK_SECRET) {
+      throw new Error(
+        "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      );
     }
-  }
-  if (eventType === "user.updated") {
-    try {
-      console.log('user.updated.up')
-      const updateData = {
-        name: `${evt.data.first_name}${evt.data.last_name ? ` ${evt.data.last_name}` : ""}`,
-        username: evt.data.username!,
-        email: evt.data.email_addresses[0].email_address,
-        picture: evt.data.image_url,
-      };
-      console.log('user.updated.mid')
-      
-      const user = await updateUser({
-        clerkId: evt.data.id,
-        path: `/profile/${evt.data.id}`,
-        updateData,
+
+    // Get the headers
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
+    console.log("\n");
+    console.log("headers", headers());
+    console.log("\n");
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return new Response("Error occured -- no svix headers", {
+        status: 400,
       });
-      console.log('user.updated.down')
-      return NextResponse.json({ message: "OK", user });
-    } catch (error) {
-      console.error("webhook.user.updated:", error);
     }
-  }
-  if (eventType === "user.deleted") {
-    try {
-      console.log('user.deleted.up')
-      const user = await deleteUser({ clerkId: evt.data.id! });
-      console.log('user.deleted.down')
-      return NextResponse.json({ message: "OK", user });
-    } catch (error) {
-      console.error("webhook.user.deleted:", error);
-    }
-  }
 
-  return new Response("", { status: 200 });
+    // Get the body
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+
+    // Create a new Svix instance with your secret.
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    let evt: WebhookEvent;
+
+    // Verify the payload with the headers
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error("Error verifying webhook:", err);
+      return new Response("Error occured", {
+        status: 400,
+      });
+    }
+
+    const eventType = evt.type;
+
+    if (eventType === "user.created") {
+      try {
+        console.log("user.created up");
+        const userData = {
+          clerkId: evt.data.id,
+          email: evt.data.email_addresses[0].email_address,
+          username: evt.data.username!,
+          name: `${evt.data.first_name} ${evt.data.last_name ? evt.data.last_name : ""}`,
+          picture: evt.data.image_url,
+        };
+        console.log("user.created mid");
+        const user = await createUser(userData);
+        console.log("user.created down");
+        return NextResponse.json({ message: "OK", user });
+      } catch (error) {
+        console.error("webhook.user.created:", error);
+      }
+    }
+    if (eventType === "user.updated") {
+      try {
+        console.log("user.updated.up");
+        const updateData = {
+          name: `${evt.data.first_name}${evt.data.last_name ? ` ${evt.data.last_name}` : ""}`,
+          username: evt.data.username!,
+          email: evt.data.email_addresses[0].email_address,
+          picture: evt.data.image_url,
+        };
+        console.log("user.updated.mid");
+
+        const user = await updateUser({
+          clerkId: evt.data.id,
+          path: `/profile/${evt.data.id}`,
+          updateData,
+        });
+        console.log("user.updated.down");
+        return NextResponse.json({ message: "OK", user });
+      } catch (error) {
+        console.error("webhook.user.updated:", error);
+      }
+    }
+    if (eventType === "user.deleted") {
+      try {
+        console.log("user.deleted.up");
+        const user = await deleteUser({ clerkId: evt.data.id! });
+        console.log("user.deleted.down");
+        return NextResponse.json({ message: "OK", user });
+      } catch (error) {
+        console.error("webhook.user.deleted:", error);
+      }
+    }
+
+    return new Response("", { status: 200 });
+  } catch (error) {
+    console.error("WEBHOOK_ERR:", error);
+  }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // add public routes here
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/",
-  "/api/webhooks(.*)",
-  "/question/:id",
-  "/tags",
-  "/tags/:id",
-  "/profile/:id",
-  "/community",
-  "/jobs",
-]);
+// const isPublicRoute = createRouteMatcher([
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+//   "/",
+//   "/api/webhooks(.*)",
+//   "/question/:id",
+//   "/tags",
+//   "/tags/:id",
+//   "/profile/:id",
+//   "/community",
+//   "/jobs",
+// ]);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) auth().protect();
+// export default clerkMiddleware((auth, request) => {
+//   if (!isPublicRoute(request)) auth().protect();
+// });
+
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
