78678183ac2e44cc9b3064462775462da34363db
Manish Suthar <manishsuthar078@gmail.com>
2024-06-25 22:50:51 +0530

    public routes added

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // add public routes here
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhooks",
+  "question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) auth().protect();
