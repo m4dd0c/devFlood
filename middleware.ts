792d2413300ce388deef4a7b1080c01827190971
Manish Suthar <manishsuthar078@gmail.com>
2024-06-27 18:47:01 +0530

    degrade clerk v5 to clerk v4

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,22 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// // add public routes here
-// // const isPublicRoute = createRouteMatcher([
-// //   "/sign-in(.*)",
-// //   "/sign-up(.*)",
-// //   "/",
-// //   "/api/webhooks(.*)",
-// //   "/question/:id",
-// //   "/tags",
-// //   "/tags/:id",
-// //   "/profile/:id",
-// //   "/community",
-// //   "/jobs",
-// // ]);
-
-// // export default clerkMiddleware((auth, request) => {
-// //   if (!isPublicRoute(request)) auth().protect();
-// // });
-
-// const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-//new
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware();
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
+  ],
+  ignoredRoutes: [
+    '/api/webhooks', '/api/chatgpt'
+  ]
+});
+ 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ 
