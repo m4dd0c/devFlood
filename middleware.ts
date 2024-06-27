bb43a40efbf41de73ac2f3fc29035f298c4b38e2
Manish Suthar <manishsuthar078@gmail.com>
2024-06-27 20:15:57 +0530

    going back clerk v4 to clerk v5, updating middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,21 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/api/webhooks',
-    '/question/:id',
-    '/tags',
-    '/tags/:id',
-    '/profile/:id',
-    '/community',
-    '/jobs'
-  ],
-  ignoredRoutes: [
-    '/api/webhooks', '/api/chatgpt'
-  ]
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhooks(.*)",
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) auth().protect();
 });
- 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
