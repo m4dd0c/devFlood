6bf6893552189de735ce13ef7ea7f99d08d2f6ae
Manish Suthar <manishsuthar078@gmail.com>
2024-06-27 20:24:07 +0530

    comment edited

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </section>
         <RightSidebar />
       </div>
-      {/* toaster */}
+      {/* toaster here */}
     </main>
   );
 };
