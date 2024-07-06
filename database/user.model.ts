d89ea44b214f2ec9fba739524fbf304c7286ef87
Manish Suthar <manishsuthar078@gmail.com>
2024-07-06 23:14:45 +0530

    timestamp true

--- database/user.model.ts
+++ database/user.model.ts
@@ -16,57 +16,60 @@ export interface IUser extends Document {
   updatedAt: NativeDate;
 }
 // : Schema<IUser>
-const UserSchema = new Schema({
-  clerkId: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    minlength: 8,
-  },
-  bio: {
-    type: String,
-  },
-  picture: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-  },
-  portfolioWebsite: {
-    type: String,
-  },
-  reputation: {
-    type: Number,
-    default: 0,
-  },
-  saved: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Question",
+const UserSchema = new Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+    },
+    bio: {
+      type: String,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    portfolioWebsite: {
+      type: String,
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
+    saved: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 // : Model<IUser>
 const User = models.User || model("User", UserSchema);
 export default User;
